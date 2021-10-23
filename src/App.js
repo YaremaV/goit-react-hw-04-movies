04d733a772c565185d7ea7364cf5ad68181517bf
YaremaV <Yaremenko1996v@gmail.com>
2021-10-23 13:22:42 +0300

    change files on directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Route, Switch } from "react-router";
-import AppBar from "./Component/AppBar/AppBar";
-import "./App.css";
+import { Route, Switch } from 'react-router';
+import AppBar from './Component/AppBar/AppBar';
+import Container from './Component/Container/Container';
+import './App.css';
 
 export default function App() {
   return (
-    <>
+    <Container>
       <AppBar />
       <Route></Route>
-    </>
+    </Container>
   );
 }
