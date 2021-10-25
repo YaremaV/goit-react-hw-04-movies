4f7332f260c1c43903ae3246925b6da65feb17d8
YaremaV <Yaremenko1996v@gmail.com>
2021-10-25 23:33:06 +0300

    Add styles for casts

--- src/views/Cast.js
+++ src/views/Cast.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import * as moviesApi from '../api-service/movies-api';
+import s from './SASS/HomeViews.module.scss';
 
 export default function MovieDetailsPage({ movieId }) {
   const [casts, setCasts] = useState([]);
@@ -9,22 +10,23 @@ export default function MovieDetailsPage({ movieId }) {
   }, [movieId]);
 
   return (
-    <>
+    <main className={s.main}>
       {casts && (
-        <ul>
+        <ul className={s.gallery}>
           {casts.map(cast => (
-            <li key={cast.id}>
+            <li key={cast.id} className={s.gallery__list}>
               <img
+                className={s.gallery__image}
                 src={`https://www.themoviedb.org/t/p/w500${cast.profile_path}`}
                 alt={cast.original_name}
               />
-              <h3>{cast.name}</h3>
-              <p>Character: {cast.character}</p>
-              <p>Popularity: {cast.popularity}</p>
+              <h3 className={s.gallery__title}>{cast.name}</h3>
+              <p className={s.gallery__text}>Character: {cast.character}</p>
+              <p className={s.gallery__text}>Popularity: {cast.popularity}</p>
             </li>
           ))}
         </ul>
       )}
-    </>
+    </main>
   );
 }
