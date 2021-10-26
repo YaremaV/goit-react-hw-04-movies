ed8dcde987a3c34950eb718454ecc90923c7186e
YaremaV <Yaremenko1996v@gmail.com>
2021-10-26 22:27:58 +0300

    fixed

--- src/views/Reviews.js
+++ src/views/Reviews.js
@@ -11,7 +11,7 @@ export default function MovieDetailsPage({ movieId }) {
 
   return (
     <>
-      {reviews ? (
+      {reviews.length ? (
         <ul className={s.reviews__list}>
           {reviews.map(review => (
             <li key={review.id} className={s.reviews__item}>
